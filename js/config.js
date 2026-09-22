e9a98495c1baa5fcc7654e94ff726b3c85a7ec9c
MarcosSouza1997 <marcostawan9779@gmail.com>
2026-09-22 20:03:42 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// js/config.js
+
+// Configurações globais de ambiente para o cliente Supabase
+window.__ENV = {
+  SUPABASE_URL: 'https://bstywcnbzbomewduqhne.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_GGJq91YrL1dCpn54-JwmpA_5rVOiYiY'
+};
+
+export const SUPABASE_URL = window.__ENV.SUPABASE_URL;
+export const SUPABASE_ANON_KEY = window.__ENV.SUPABASE_ANON_KEY;
